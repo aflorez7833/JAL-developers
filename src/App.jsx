390dbf335d50b8f732e875dc13f99d2e0ae09edd
aflorez7833 <aliforez7833@gmail.com>
2021-10-02 15:08:05 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@
 
 import Pagina1 from './pages/pagina1';
 import Pagina2 from './pages/pagina2';
+import Pagina4 from './pages/pagina4';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './styles/styles.css';
+import './styles/stylesali.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
+            <Route path='/pagina4'>
+              <Pagina4 />
+            </Route>
             <Route path='/pagina2'>
               <Pagina1 />
             </Route>
---
+++ src/pages/pagina4.jsx
@@ -0,0 +1,58 @@
+
+import '../styles/stylesali.css';
+
+
+function Pagina4() {
+    return (
+        <div>
+        <head>
+            <title>Modulo Administracion de Ventas</title>
+        </head>
+        <body>
+                           
+            <h1 className="title">Administracion De Ventas</h1>
+            
+                <form action="forma">
+                    <label htmlFor="text">Ingrese La Fecha de La Venta   </label>
+                    <input type="date" />
+                </form>
+                <form action="forma">
+                    <label htmlFor="text">Codigo Vendedor   </label>
+                    <input type="Number" />
+                </form>            
+                <form action="forma">
+                    <label htmlFor="fname">Nombre y Apellidos del Cliente   </label>
+                    <input type="text" />
+                </form>
+                <form action="forma">
+                    <label htmlFor="text">Numero de Identificacion   </label>
+                    <input type="number" />
+                </form>
+                <form action="forma">
+                    <label htmlFor="email">Email Cliente  </label>
+                    <input type="email" />
+                </form>
+                <form action="forma">
+                    <label htmlFor="text">Numero del Pedido   </label>
+                    <input type="number" />
+                </form>
+                <form action="forma">
+                    <label htmlFor="text">Valor Total del Pedido   </label>
+                    <input type="currency" />
+                </form>
+                <form action="forma">
+                    <label htmlFor="text">Ingrese Fecha de Pago   </label>
+                    <input type="date" />
+                </form>
+                <form action="forma">
+                    <input type="submit" />
+                </form>
+                        
+        </body>            
+        </div>
+        
+               
+    );
+  }
+  
+  export default Pagina4;
