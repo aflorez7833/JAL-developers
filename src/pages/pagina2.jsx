feb6b759d099e59ae259e807e111eb11ea79e899
JhonatanStevenMonroyGallegos <jhonatan.estiben.monroy1@gmail.com>
2021-10-03 12:46:45 -0500

    gestion de usuario por jhonny

--- src/pages/pagina2.jsx
+++ src/pages/pagina2.jsx
@@ -1,9 +1,15 @@
-
+import '../styles/stylesjhonny.css'
+import fv from '../media/vendedor.png'
+import fa from '../media/administrador.png'
 
 function Pagina2() {
     return (
-      <div className="App">
-          <h1>pagina de inicio</h1>
+      <div className="GU">
+          <h1>Getión de usuarios</h1>
+          <img id='vendi' src={fv} alt="vendedor" />
+          <img id='admi' src={fa} alt="administrador" />
+          <div>su estado actual es  es: pendiente/autorizado/no autorizado</div>
+          <span>Actualizar</span> 
       </div>
     );
   }
